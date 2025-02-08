35e257f2f9780271b09ab7f2ab704d725951bc2e
James <james.savona@outlook.com>
2025-02-08 19:41:13 +1100

    Finish user signup

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module SampleApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Sydney"
+
+    
   end
 end
