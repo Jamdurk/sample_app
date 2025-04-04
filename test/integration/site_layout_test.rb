210b3ea979e67b181d87216c1a44874a3f19cf87
James <james.savona@outlook.com>
2025-04-04 17:17:34 +1100

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -17,6 +17,8 @@ test "layout links" do
   assert_select "a[href=?]", login_path
   assert_select "a[href=?]", logout_path, count: 0
   assert_select "a[href=?]", signup_path, count: 1
+  assert_no_match "following", response.body
+  assert_no_match "followers", response.body
   assert_select "a[href=?]", user_path(@user), count: 0
   get contact_path
   assert_select "title", full_title("Contact")
@@ -38,6 +40,8 @@ test "layout links" do
   assert_select "a[href=?]", logout_path
   assert_select "a[href=?]", edit_user_path(@user)
   assert_select "a[href=?]", user_path(@user)
+  assert_match @user.following.count.to_s, response.body
+  assert_match @user.followers.count.to_s, response.body
 end
 
 
